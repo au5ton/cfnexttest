2c83f99374e9cb4e14474d3ee44896db6c97d92a
Austin Jackson <themacphage@gmail.com>
2025-07-24 21:00:51 -0500

    uh maybe

--- src/pages/api/hello.ts
+++ src/pages/api/hello.ts
@@ -1,13 +1,21 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from "next";
+import { env } from 'cloudflare:workers'
 
-type Data = {
-  name: string;
-};
+// type Data = {
+//   name: string;
+// };
+
+type Data = { [key: string]: any }
 
 export default function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>,
 ) {
-  res.status(200).json({ name: "John Doe" });
+  res.status(200).json({
+    name: "John Doe",
+    NEXTJS_ENV: env.NEXTJS_ENV,
+    EXAMPLE_VARIABLE: env.EXAMPLE_VARIABLE,
+    EXAMPLE_SECRET: env.EXAMPLE_SECRET,
+  });
 }
