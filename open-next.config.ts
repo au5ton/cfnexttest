f619f01881519324ea9c02c228ab3e55732a028e
Austin Jackson <themacphage@gmail.com>
2025-07-24 21:22:41 -0500

    test

--- open-next.config.ts
+++ open-next.config.ts
@@ -6,5 +6,5 @@ export default defineCloudflareConfig({
   // It should be imported as:
   // `import r2IncrementalCache from "@opennextjs/cloudflare/overrides/incremental-cache/r2-incremental-cache";`
   // See https://opennext.js.org/cloudflare/caching for more details
-  incrementalCache: r2IncrementalCache,
+  //incrementalCache: r2IncrementalCache,
 });
